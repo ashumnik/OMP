8f1e9e334ae07f06b9bcfc193eff6f6bc3a9096b
readysloth <readysloth@protonmail.com>
2019-08-26 18:37:53 +0300

    Добавил красивый вывод

--- Lab3/Task1.cpp
+++ Lab3/Task1.cpp
@@ -8,73 +8,67 @@
 #include <cstdlib>
 #include <ctime>
 #define OUTPUT
-#define M 10
-#define N 20
-#define N_h N/2
+#define PARALLEL
+#define N 40
+#define LF 20
 
-int matrix[M][N];
-int matrix_computed[M/2][N/2] = 0;
+int A[N] = {0};
+int B[N] = {0};
+int C[N] = {0};
 
-int mul(int a, int b){
-    return a*b;
-}
+void print_matrix(int matrix[N]){
+    #ifdef OUTPUT
+    for(int k = 0; k < N ; k+=LF){
+        std::cout << std::setw(3) << "";
 
-int sum(int a, int b){
-    return a+b;
-}
-
-long long bits(int a){
-    return std::bitset<sizeof(decltype(matrix[0][0]))*8>(a).count();
-}
-
-long long sevens(int a){
-    long long count = 0;
-
-    while(a > 0){
-        if(a % 10 == 7){
-            count++;
+        for(int i = k; i < k+LF; i++){
+            std::cout << std::setw(5) << std::setfill('_') << i << '|';
         }
-        a /= 10;
-    }
-    
-    return count;
-}
-
-void init(){
-    std::srand(unsigned(std::time(0)));
-    for(int i = 0; i < M; i++){
-        for(int j = 0; j < N; j++){
-             matrix[i][j] = std::rand()%M+1;
-             #ifdef OUTPUT
-             std::cout << std::setw(5) << matrix[i][j];
-             #endif
-        }
-        #ifdef OUTPUT
         std::cout << std::endl;
-        #endif
-    }
-}
 
-long long compute(int (op*)(int,int), long long (amount)(int)){
-    long long result = 0;
-    for(int i = 0; i < M; i++){
-        for(int j = 0, j_c = 0; j < N_h; j+=2, j_c++){
-             matrix_computed[i][j_c] = op(matrix[i][j], matrix[i][j+1]); 
-             // atomic
-             result += amount(matrix_computed[i][j_c]);
+        std::cout << std::setfill(' ');
+        std::cout << std::setw(3) << "";
+
+        for(int i = k; i < k+LF; i++){
+            std::cout << std::setw(5) << matrix[i] << '|';
         }
+        std::cout << std::endl;
+        std::cout << std::endl;
+    }
+    #endif
+    
+}
+void init(int matrix[N]){
+    std::srand(unsigned(std::time(0)));
+#ifdef PARALLEL
+    #pragma omp parallel for
+#endif
+    for(int i = 0; i < N; i++){
+        matrix[i] = std::rand()%N+1;
     }
 }
 
 int main(){
-    init();
-    std::chrono::time_point<std::chrono::high_resolution_clock> start;
-    start = std::chrono::high_resolution_clock.now()
+    init(A);
+    init(B);
 
-    compute(mul,bits);
+    std::cout << std::setw(3) << "";
+    std::cout << "vvvvv A vvvvv" << std::endl;
 
-    auto elapsed = start.time_since_epoch();
-    std::cout << elapsed.count() << "nanoseconds" << std::endl;
+    print_matrix(A);
+
+    std::cout << std::setw(3) << "";
+    std::cout << "vvvvv B vvvvv" << std::endl;
+
+    print_matrix(B);
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    start = std::chrono::high_resolution_clock::now();
+
+
+    end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>( end - start ).count();
+    std::cout << elapsed << " microseconds" << std::endl;
 
     
 
