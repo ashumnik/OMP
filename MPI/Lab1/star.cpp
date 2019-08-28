7f164c2a26abb4858b36ad0530c949c33b34b5c9
readysloth <readysloth@protonmail.com>
2019-08-28 21:56:38 +0300

    Разнес варианты по разным файлам, так легче организовать код

---
+++ MPI/Lab1/star.cpp
@@ -0,0 +1,76 @@
+#include "mpi.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include "star.hpp"
+
+void star(std::string message_to_root, std::string message_to_edges){
+    
+    char recieved[256] = {0}; 
+
+    int world_size;
+    int rank;
+    MPI_Init(nullptr, nullptr);
+    
+    // Получаем количество процессов и их текущий ранг
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    if(rank == 0){
+        std::cout << "world size is: " << world_size << std::endl;
+    }
+
+    int next_proc = rank+1;
+    
+    for(int round = M; round > 0; round--){
+        std::cout << std::setfill('-') << std::setw(40) << "Current round is " << round << std::endl;
+
+        if(rank == 0){
+            for(int side_proc = 1; cnt < world_size; cnt++){
+                // Послать сообщение всем крайним процессам
+                MPI_Send(message_to_edges.c_str(),
+                         message_to_edges.size()+1, //+1 из-за '\0' 
+                         MPI_CHAR, 
+                         side_proc, 
+                         0,
+                         MPI_COMM_WORLD); 
+            }
+            for(int side_proc = 1; cnt < world_size; cnt++){
+                // Принять сообщения от всех крайних процессов
+                MPI_Recv(recieved, 
+                         sizeof(recieved), 
+                         MPI_CHAR,
+                         side_proc,
+                         0,
+                         MPI_COMM_WORLD,
+                         MPI_STATUS_IGNORE);
+                std::cout << "It's root process, and i recieved this:{" << recieved << "} from [" << side_proc << "]"  << std::endl;
+            }
+        }
+        else{
+            // Принять сообщение от главного процесса
+            MPI_Recv(recieved, 
+                     sizeof(recieved), 
+                     MPI_CHAR,
+                     0,
+                     0,
+                     MPI_COMM_WORLD,
+                     MPI_STATUS_IGNORE);
+
+            std::cout << "It's " << rank << " process, and i recieved this:{" << recieved << "}" << std::endl;
+
+            // Послать сообщение главному процессу
+            MPI_Send(message_to_root.c_str(),
+                     message_to_root.size()+1, //+1 из-за '\0' 
+                     MPI_CHAR, 
+                     0, 
+                     0,
+                     MPI_COMM_WORLD); 
+        }
+
+    }
+
+    
+    MPI_Finalize();
+}
