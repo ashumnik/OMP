bddb83c2915b2fbabcd6459225e987e2505bc44a
readysloth <readysloth@protonmail.com>
2019-09-08 17:34:51 +0300

    Сделал вторую лабораторную и исправил star.cpp в первой

---
+++ MPI/Lab2/ring.hpp
@@ -0,0 +1,10 @@
+#ifndef HPP_RING
+#define HPP_RING
+
+#include <string>
+#define N 10
+#define M 5
+
+void ring(std::string message);
+
+#endif
--- MPI/Lab2/star.cpp
+++ MPI/Lab2/star.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <cstring>
 #include "star.hpp"
+
+#define D(X) (std::cout << X << std::endl)
 
 void star(std::string message_to_root, std::string message_to_edges){
     
     char recieved[256] = {0}; 
+    char broadcast_buffer[256] = {0}; 
 
     int world_size;
     int rank;
@@ -26,15 +30,16 @@ void star(std::string message_to_root, std::string message_to_edges){
         std::cout << std::setfill('-') << std::setw(40) << "Current round is " << round << " in [" << rank << "]" << std::endl;
 
         if(rank == 0){
-            for(int side_proc = 1; side_proc < world_size; side_proc++){
-                // Послать сообщение всем крайним процессам
-                MPI_Send(message_to_edges.c_str(),
-                         message_to_edges.size()+1, //+1 из-за '\0' 
-                         MPI_CHAR, 
-                         side_proc, 
-                         0,
-                         MPI_COMM_WORLD); 
+            if(round == M){
+                std::strcpy(broadcast_buffer, message_to_edges.c_str());
             }
+            // Послать сообщение всем крайним процессам
+            MPI_Bcast(
+                     broadcast_buffer,
+                     message_to_edges.size()+1, //+1 из-за '\0' 
+                     MPI_CHAR, 
+                     0, 
+                     MPI_COMM_WORLD); 
             for(int side_proc = 1; side_proc < world_size; side_proc++){
                 // Принять сообщения от всех крайних процессов
                 MPI_Recv(recieved, 
@@ -51,13 +56,13 @@ void star(std::string message_to_root, std::string message_to_edges){
         }
         else{
             // Принять сообщение от главного процесса
-            MPI_Recv(recieved, 
-                     sizeof(recieved), 
-                     MPI_CHAR,
-                     0,
-                     0,
-                     MPI_COMM_WORLD,
-                     MPI_STATUS_IGNORE);
+            MPI_Bcast(
+                     broadcast_buffer,
+                     message_to_edges.size()+1, //+1 из-за '\0' 
+                     MPI_CHAR, 
+                     0, 
+                     MPI_COMM_WORLD); 
+            D("children recv");
 
             std::cout << "It's " << rank << " process, and i recieved this:{" << recieved << "}" << std::endl;
 
