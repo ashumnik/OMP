1a4a1dbef8bfa5f265ae277ba872e9354511c1ef
readysloth <readysloth@protonmail.com>
2019-10-10 13:24:44 +0300

    Лаба сделана

--- MPI/Lab4_header/Task1.cpp
+++ MPI/Lab4_header/Task1.cpp
@@ -16,11 +16,17 @@ int main(int argc, char *argv[]){
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
 
-    const std::size_t size = 100;
+    const std::size_t size = 4096;
     LongInt<size> l_int1(1);
     LongInt<size> l_int2(2);
+    
+    double start = MPI_Wtime();
+
     auto l_int3 = l_int1 * l_int2;
     
+    double end = MPI_Wtime();
+
+    /*
     if(rank == 0)
     std::cout << l_int1.ToString() 
               << " * " 
@@ -28,8 +34,12 @@ int main(int argc, char *argv[]){
               << " = " 
               << l_int3.ToString()
               << std::endl;
+              */
 
     MPI_Finalize();
+
+    if(rank == 0)
+        std::cout << "Multiplication took: " << end-start << "secs" << std::endl; 
 
     return 0;
 }
